8655f5eaba643a78c81d447f0b900019b7ef489a
akegata <akegata@puppet.akegata.se>
2016-02-06 23:02:32 +0100

    Added docker.

--- docker/manifests/init.pp
+++ docker/manifests/init.pp
@@ -62,41 +62,33 @@ class docker (
           }
         }
         '7': {
-          package { 'docker':
-            ensure => 'absent',
+          package { 'docker-engine':
+            ensure => 'present',
           }
     
-          file { '/usr/bin/docker':
-            ensure  => 'present',
-            source  => 'puppet:///modules/docker/docker',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0555',
-          }
-    
-          file { '/usr/lib/systemd/system/docker.service':
-            ensure  => 'present',
-            source  => 'puppet:///modules/docker/docker.service',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0755',
-          }
-    
-          file { '/usr/lib/systemd/system/docker.socket':
-            ensure  => 'present',
-            source  => 'puppet:///modules/docker/docker.socket',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0755',
-          }
-    
-          file { '/etc/bash_completion.d/docker.bash':
-            ensure  => 'present',
-            source  => 'puppet:///modules/docker/docker.bash',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0644',
-          }
+          #file { '/usr/lib/systemd/system/docker.service':
+         #   ensure  => 'present',
+         #   source  => 'puppet:///modules/docker/docker.service',
+         #   owner   => 'root',
+         #   group   => 'root',
+         #   mode    => '0755',
+         # }
+   # 
+   #       file { '/usr/lib/systemd/system/docker.socket':
+   #         ensure  => 'present',
+   #         source  => 'puppet:///modules/docker/docker.socket',
+   #         owner   => 'root',
+   #         group   => 'root',
+   #         mode    => '0755',
+   #       }
+   # 
+   #       file { '/etc/bash_completion.d/docker.bash':
+   #         ensure  => 'present',
+   #         source  => 'puppet:///modules/docker/docker.bash',
+   #         owner   => 'root',
+   #         group   => 'root',
+   #         mode    => '0644',
+   #       }
     
           file { '/usr/share/vim/vimfiles/syntax/dockerfile.vim':
             ensure  => 'present',
